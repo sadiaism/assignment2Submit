853e097311896c26bd84d495c2e2e268d80ff931
sadiaism <ismailmoiz80@gmail.com>
2025-02-13 01:04:16 +0500

    the ass 2

--- src/app/Components/Section/Section.tsx
+++ src/app/Components/Section/Section.tsx
@@ -1,10 +1,14 @@
 import cssstyle from "./Section.module.css"
 
-const Section=(props:any) =>{
+interface SectionProps {
+    page: React.ReactNode;  // Define a specific type
+  }
+
+const Section=({page}: SectionProps) =>{
     return(
         <section>
             <h1 className="flex justify-center text-[32px] font-bold">Hero Section</h1>
-            <div className={cssstyle.HeroSection} >{props.page}
+            <div className={cssstyle.HeroSection} >{page}
                
             </div>
         </section>
